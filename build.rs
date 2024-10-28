cd817e374ec0ec9657b1b7e519261bd364cac458
Gabriel de Perthuis <g2p.code@gmail.com>
2024-10-28 10:42:57 +0000

    Add a table of system dependencies with minimum versions
    This documents and enforces minimum requirements for C library
    dependencies.  At the moment versions were picked from debian bookworm
    (debian stable since 2023-06), except blkid (util-linux) where 2.40.1
    is required:
    https://github.com/koverstreet/bcachefs-tools/pull/347
    
    libaio is excluded because it doesn't provide a pkg-config file.
    
    Also remove libscrypt from debian/control since we use the libsodium
    implementation.
    
    Signed-off-by: Gabriel de Perthuis <g2p.code@gmail.com>

--- build.rs
+++ build.rs
@@ -3,19 +3,8 @@ fn main() {
     println!("cargo:rerun-if-changed=libbcachefs.a");
     println!("cargo:rustc-link-lib=static:+whole-archive=bcachefs");
 
-    println!("cargo:rustc-link-lib=urcu");
-    println!("cargo:rustc-link-lib=zstd");
-    println!("cargo:rustc-link-lib=blkid");
-    println!("cargo:rustc-link-lib=uuid");
-    println!("cargo:rustc-link-lib=sodium");
-    println!("cargo:rustc-link-lib=z");
-    println!("cargo:rustc-link-lib=lz4");
-    println!("cargo:rustc-link-lib=zstd");
-    println!("cargo:rustc-link-lib=udev");
-    println!("cargo:rustc-link-lib=keyutils");
+    // libaio is missing a pkg-config file
     println!("cargo:rustc-link-lib=aio");
 
-    if std::env::var("BCACHEFS_FUSE").is_ok() {
-        println!("cargo:rustc-link-lib=fuse3");
-    }
+    system_deps::Config::new().probe().unwrap();
 }
