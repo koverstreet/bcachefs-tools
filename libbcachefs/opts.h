d42abfef01198d333a39123121e01e3e2d05f43c
Eelco Wesemann <sgaduuw@users.noreply.github.com>
2025-03-07 19:44:49 +0100

    Remove superfluous space

--- libbcachefs/opts.h
+++ libbcachefs/opts.h
@@ -519,7 +519,7 @@ enum fsck_err_opts {
 	  OPT_BOOL(),							\
 	  BCH2_NO_SB_OPT,		true,				\
 	  NULL,		"BTREE_ITER_prefetch causes btree nodes to be\n"\
-	  " prefetched sequentially")
+	  "prefetched sequentially")
 
 #define BCH_DEV_OPT_SETTERS()						\
 	x(discard,		BCH_MEMBER_DISCARD)			\
