2a1808a07083ca2cd9ee3523986d37327f4ccf2a
Ivan Shapovalov <intelfx@intelfx.name>
2026-03-17 15:46:13 +0100

    bdev: add get_blocksize_logical(), for future usage

--- src/wrappers/bdev.rs
+++ src/wrappers/bdev.rs
@@ -9,6 +9,7 @@ use std::os::unix::io::RawFd;
 
 use libc::{
     BLKPBSZGET,
+    BLKSSZGET,
 };
 
 // linux/fs.h ioctl constants not exposed by libc crate
@@ -40,6 +41,21 @@ pub fn get_blocksize_physical_hint(fd: RawFd) -> u32 {
         let mut bs: libc::c_uint = 0;
         unsafe { libc::ioctl(fd, BLKPBSZGET, &mut bs) };
         bs
+    } else {
+        stat.st_blksize as u32
+    }
+}
+
+/// Returns logical block size (LBA) of a block device (the smaller of the two),
+/// with fallback to the filesystem block size hint for regular files, in bytes.
+/// (suitable for use as an alignment for direct I/O or similar)
+pub fn get_blocksize_logical(fd: RawFd) -> u32 {
+    let stat = fstat(fd);
+
+    if is_blk(stat.st_mode) {
+        let mut bs: libc::c_uint = 0;
+        unsafe { libc::ioctl(fd, BLKSSZGET, &mut bs) };
+        bs as u32
     } else {
         stat.st_blksize as u32
     }
