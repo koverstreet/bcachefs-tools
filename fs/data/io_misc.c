koverstreet/bcachefs-tools

commit a626670b8566152b2aa6b5101c6536e993b68071

bcachefs: Fix finsert/fcollapse disk reservations for snapshots
Link: https://www.reddit.com/r/bcachefs/comments/1puiuyj/falloc_fl_insert_range_with_snapshot/
Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/data/io_misc.c
@@ -426,8 +426,15 @@ case LOGGED_OP_FINSERT_shift_extents:
 		if ((ret = PTR_ERR_OR_ZERO(copy)))
 			goto btree_err;
 
-		if (insert &&
-		    bkey_lt(bkey_start_pos(k.k), src_pos)) {
+		if (snapshot != k.k->p.snapshot) {
+			ret = bch2_disk_reservation_add(c, &disk_res,
+					copy->k.size *
+					bch2_bkey_nr_ptrs_allocated(c, bkey_i_to_s_c(copy)),
+					0);
+			if (ret)
+				goto btree_err;
+		} else if (insert &&
+			   bkey_lt(bkey_start_pos(k.k), src_pos)) {
 			bch2_cut_front(c, src_pos, copy);
 
 			/* Splitting compressed extent? */

commit 0b1236dba9157360255cf1b1a493fc136fc7b85c

fcollapse: trim extents that straddle range start
Fixes: a626670b "Fix finsert/fcollapse disk reservations for snapshots"

fs/data/io_misc.c
@@ -438,15 +438,17 @@ case LOGGED_OP_FINSERT_shift_extents:
 					0);
 			if (ret)
 				goto btree_err;
-		} else if (insert &&
-			   bkey_lt(bkey_start_pos(k.k), src_pos)) {
+		}
+
+		if (bkey_lt(bkey_start_pos(k.k), src_pos)) {
 			bch2_cut_front(c, src_pos, copy);
 
 			/* Splitting compressed extent? */
-			bch2_disk_reservation_add(c, &disk_res,
-					copy->k.size *
-					bch2_bkey_nr_ptrs_allocated(c, bkey_i_to_s_c(copy)),
-					BCH_DISK_RESERVATION_NOFAIL);
+			if (snapshot == k.k->p.snapshot)
+				bch2_disk_reservation_add(c, &disk_res,
+							  copy->k.size *
+							  bch2_bkey_nr_ptrs_allocated(c, bkey_i_to_s_c(copy)),
+							  BCH_DISK_RESERVATION_NOFAIL);
 		}
 
 		bkey_init(&delete.k);