6ea6dc5d68133debcd2d15d0980bec22a6dbe374
Kent Overstreet <kent.overstreet@linux.dev>
2026-06-16 18:03:29 -0500

    rust conversion: use safe wrappers for inode operations

--- fs/fs/namei.rs
+++ fs/fs/namei.rs
@@ -30,3 +30,38 @@ pub fn unlink_trans(
         c::bch2_unlink_trans(trans.raw(), dir_inum, dir, target, inode, name, deleting)
     })
 }
+
+#[allow(clippy::too_many_arguments)]
+pub fn create_trans(
+    trans:        &BtreeTrans,
+    dir_inum:     c::subvol_inum,
+    dir:          &mut c::bch_inode_unpacked,
+    inode:        &mut c::bch_inode_unpacked,
+    subvol:       &mut c::bch_subvolume,
+    name:         &c::qstr,
+    uid:          c::uid_t,
+    gid:          c::gid_t,
+    mode:         c::umode_t,
+    rdev:         c::dev_t,
+    snapshot_src: c::subvol_inum,
+    flags:        u32,
+) -> Result<(), BchError> {
+    ret_to_result(unsafe {
+        c::bch2_create_trans(
+            trans.raw(),
+            dir_inum,
+            dir,
+            inode,
+            subvol,
+            name,
+            uid,
+            gid,
+            mode,
+            rdev,
+            core::ptr::null_mut(),
+            core::ptr::null_mut(),
+            snapshot_src,
+            flags,
+        )
+    })
+}
