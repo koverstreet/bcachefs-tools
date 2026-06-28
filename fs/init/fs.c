845d741b5a670984c4d2852ce2c41c6d28c93c7d
Darafei Praliaskouski <me@komzpa.net>
2026-06-28 19:44:52 +0400

    bcachefs: avoid startup reconcile rescan

--- fs/init/fs.c
+++ fs/init/fs.c
@@ -612,12 +612,10 @@ static int __bch2_fs_read_write(struct bch_fs *c, bool early)
 
 	int ret = bch2_journal_reclaim_start(&c->journal) ?:
 		  bch2_btree_write_buffer_start(c) ?:
-		  bch2_copygc_start(c);
-	if (!ret && c->opts.background_target &&
-	    c->opts.background_target != c->opts.foreground_target)
-		ret = bch2_set_fs_needs_reconcile(c);
-	if (!ret && !c->opts.read_only)
-		ret = bch2_reconcile_start(c);
+		  bch2_copygc_start(c) ?:
+		  (!c->opts.read_only
+		   ? bch2_reconcile_start(c)
+		   : 0);
 	if (ret) {
 		bch2_fs_read_only(c);
 		return ret;
