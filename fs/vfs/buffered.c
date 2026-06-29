3ea9714aed77727a5789ecd20ff0db7f4d457198
Darafei Praliaskouski <me@komzpa.net>
2026-06-29 08:27:22 +0400

    alloc: reclaim partial buckets before writeback throttle

--- fs/vfs/buffered.c
+++ fs/vfs/buffered.c
@@ -542,6 +542,8 @@ static bool can_write_now(struct bch_fs *c, unsigned replicas_want, struct closu
 	unsigned reserved = OPEN_BUCKETS_COUNT -
 		(OPEN_BUCKETS_COUNT - bch2_open_buckets_reserved(BCH_WATERMARK_normal)) / 2;
 
+	bch2_open_bucket_reclaim_unused_partials(c, reserved);
+
 	if (unlikely(c->allocator.open_buckets_nr_free <= reserved)) {
 		closure_wait(&c->allocator.open_buckets_wait, cl);
 		return false;
